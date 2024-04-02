c361f1c1d09ef7c7ddd013a1570dbdef69bbdf50
정영일 <bbangwon@outlook.com>
2024-04-02 22:46:06 +0900

    움직임 애니메이션 적용

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,13 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
     NavMeshAgent navMeshAgent;
+    Animator animator;
 
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
@@ -16,6 +19,7 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -26,5 +30,14 @@ public class Mover : MonoBehaviour
             hit.point = new Vector3(hit.point.x, 0, hit.point.z);
             navMeshAgent.SetDestination(hit.point);
         }
+    }
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = navMeshAgent.velocity;
+        //로컬 방향 벡터로 변환
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        //z축(foward) 크기가 속도
+        float speed = localVelocity.z;
+        animator.SetFloat("forwardSpeed", speed);
     }
 }
