81c1162108ded766815eeaae067b433839aff8fe
youngil <youngil@placeb.com>
2024-04-04 09:49:51 +0900

    컴포넌트에 대한 레이캐스팅

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,4 +1,6 @@
+using RPG.Combat;
 using RPG.Movement;
+using System;
 using UnityEngine;
 
 namespace RPG.Control
@@ -6,14 +8,40 @@ namespace RPG.Control
     public class PlayerController : MonoBehaviour
     {
         Mover mover;
+        Fighter fighter;
 
         private void Awake()
         {
             mover = GetComponent<Mover>();
+            fighter = GetComponent<Fighter>();
         }
 
         // Update is called once per frame
         void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                //hit.transform을 사용하면 rigidbody또는 collider를 모두 사용하여 감지함
+                if(hit.transform.TryGetComponent(out CombatTarget target))
+                {
+                    if (target == null) continue;
+
+                    if(Input.GetMouseButtonDown(0))
+                    {
+                        fighter.Attack(target);
+                    }
+                }
+            }
+        }
+
+        private void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
@@ -23,12 +51,15 @@ namespace RPG.Control
 
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            if (Physics.Raycast(GetMouseRay(), out RaycastHit hit))
             {
                 mover.MoveTo(hit.point);
             }
         }
-    }
 
+        private Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+    }
 }
