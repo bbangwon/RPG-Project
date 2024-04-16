6e19775425236209f073122f4c116d5ce17b9ea8
정영일 <bbangwon@outlook.com>
2024-04-17 01:12:14 +0900

    C#에서 바이너리로 변환하기

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Text;
 using UnityEngine;
@@ -11,8 +12,14 @@ namespace RPG.Saving
             string path = GetPathFromSaveFile(saveFile);
             Debug.Log("Would save to " + path);
             using FileStream stream = File.Open(path, FileMode.Create);
-            byte[] bytes = Encoding.UTF8.GetBytes("Çï·Î¿ì ¿ùµå!");
-            stream.Write(bytes, 0, bytes.Length);
+            Transform playerTransform = GetPlayerTransform();
+            byte[] buffer = SerialzeVector(playerTransform.position);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+
+        private Transform GetPlayerTransform()
+        {
+            return GameObject.FindWithTag("Player").transform;
         }
 
         public void Load(string saveFile)
@@ -22,8 +29,26 @@ namespace RPG.Saving
             using FileStream stream = File.Open(path, FileMode.Open);
             byte[] buffer = new byte[stream.Length];
             stream.Read(buffer, 0, buffer.Length);
+            Transform playerTransform = GetPlayerTransform();
+            playerTransform.position = DeserializeVector(buffer);
+        }
 
-            Debug.Log(Encoding.UTF8.GetString(buffer));
+        private byte[] SerialzeVector(Vector3 vector)
+        {
+            byte[] bytes = new byte[3 * 4];
+            BitConverter.GetBytes(vector.x).CopyTo(bytes, 0);
+            BitConverter.GetBytes(vector.y).CopyTo(bytes, 4);
+            BitConverter.GetBytes(vector.z).CopyTo(bytes, 8);
+            return bytes;
+        }
+
+        private Vector3 DeserializeVector(byte[] buffer)
+        {
+            Vector3 result = new Vector3();
+            result.x = BitConverter.ToSingle(buffer, 0);
+            result.y = BitConverter.ToSingle(buffer, 4);
+            result.z = BitConverter.ToSingle(buffer, 8);
+            return result;
         }
 
         private string GetPathFromSaveFile(string saveFile)
