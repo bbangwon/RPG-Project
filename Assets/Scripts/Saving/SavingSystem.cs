00130e00d9fa7d3a50b95b8022b193795124da8c
정영일 <bbangwon@outlook.com>
2024-04-21 01:20:02 +0900

    여러 씬 저장하기 리펙토링

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,7 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
-using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace RPG.Saving
@@ -10,29 +11,40 @@ namespace RPG.Saving
     {
         public void Save(string saveFile)
         {
+            SaveFile(saveFile, CaptureState());
+        }
+
+        public void Load(string saveFile)
+        {
+            RestoreCapture(LoadFile(saveFile));
+        }
+
+        private void SaveFile(string saveFile, Dictionary<string, object> dictionary)
+        {
             string path = GetPathFromSaveFile(saveFile);
             Debug.Log("Would save to " + path);
             using FileStream stream = File.Open(path, FileMode.Create);
 
-            JsonSerializer.Serialize(stream, CaptureState(), 
-                new JsonSerializerOptions { 
-                    WriteIndented = true
-                });
+            JsonSerializer.Serialize(stream, dictionary,
+                               new JsonSerializerOptions
+                               {
+                                   WriteIndented = true
+                               });
         }
 
-        public void Load(string saveFile)
+        private Dictionary<string, object> LoadFile(string saveFile)
         {
             string path = GetPathFromSaveFile(saveFile);
             Debug.Log("Would load from " + path);
             using FileStream stream = File.Open(path, FileMode.Open);
 
-            RestoreCapture(JsonSerializer.Deserialize<Dictionary<string, object>>(stream));
+            return JsonSerializer.Deserialize<Dictionary<string, object>>(stream);
         }
 
         private Dictionary<string, object> CaptureState()
         {
             Dictionary<string, object> state = new Dictionary<string, object>();
-            foreach(SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
+            foreach (SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
             {
                 state[saveable.GetUniqueIdentifier()] = saveable.CaptureState();
             }
@@ -41,10 +53,10 @@ namespace RPG.Saving
 
         private void RestoreCapture(Dictionary<string, object> state)
         {
-            foreach(SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
+            foreach (SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
             {
                 string id = saveable.GetUniqueIdentifier();
-                if(state.ContainsKey(id))
+                if (state.ContainsKey(id))
                 {
                     saveable.RestoreState(state[id]);
                 }
