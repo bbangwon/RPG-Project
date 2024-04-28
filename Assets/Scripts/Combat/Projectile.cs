c8386eb568e88cd861c794578946580d70a1a19b
정영일 <bbangwon@outlook.com>
2024-04-29 00:20:17 +0900

    발사체 동작 개선하기

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -6,16 +6,26 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float speed = 1f;
+        [SerializeField] bool isHoming = false;
         CombatTarget target;
         float damage = 0;
 
         CapsuleCollider targetCapsuleCollider;
 
+        private void Start()
+        {
+            transform.LookAt(GetAimLocation());
+        }
+
         void Update()
         {
             if(target == null) return;
 
-            transform.LookAt(GetAimLocation());
+            if(isHoming && !target.IsDead())
+            {
+                transform.LookAt(GetAimLocation());
+            }
+
             transform.Translate(speed * Time.deltaTime * Vector3.forward);
         }
 
@@ -40,6 +50,7 @@ namespace RPG.Combat
         private void OnTriggerEnter(Collider other)
         {
             if (other.GetComponent<CombatTarget>() != target) return;
+            if (target.IsDead()) return;
             target.TakeDamage(damage);
             Destroy(gameObject);
         }
