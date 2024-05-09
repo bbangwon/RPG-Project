8dcdff3fa6fc11b8b46003e1059f6775376b8177
정영일 <bbangwon@outlook.com>
2024-05-10 00:25:56 +0900

    레벨 디스플레이

---
+++ Assets/Scripts/Stats/LevelDisplay.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace RPG.Stats
+{
+    public class LevelDisplay : MonoBehaviour
+    {
+        BaseStats baseStats;
+        TextMeshProUGUI tmpText;
+
+        private void Awake()
+        {
+            baseStats = GameObject.FindWithTag("Player").GetComponent<BaseStats>();
+            tmpText = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Update()
+        {
+            tmpText.text = $"{baseStats.GetLevel():0}";
+        }
+    }
+
+}
