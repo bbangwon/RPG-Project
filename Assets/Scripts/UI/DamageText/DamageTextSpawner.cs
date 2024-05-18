380e5b07bcd645829e9dd43effd13a75ca9297c0
정영일 <bbangwon@outlook.com>
2024-05-18 10:30:39 +0900

    데미지 텍스트 생성하기

---
+++ Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace RPG.UI.DamageText
+{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] DamageText damageTextPrefab;
+
+        private void Start()
+        {
+            Spawn(10);            
+        }
+
+        public void Spawn(float damageAmount)
+        {
+            DamageText damageText = Instantiate(damageTextPrefab, transform);
+        }
+    }
+
+}
