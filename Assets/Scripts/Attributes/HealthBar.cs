b7be909fb75e79c259d222a5a6bd2cfe171e5cb9
정영일 <bbangwon@outlook.com>
2024-05-19 21:22:22 +0900

    빈 체력 바 비활성화하기

--- Assets/Scripts/Attributes/HealthBar.cs
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -6,9 +6,17 @@ namespace RPG.Attributes
     {
         [SerializeField] Health healthComponent;
         [SerializeField] RectTransform foreground;
+        [SerializeField] Canvas rootCanvas;
 
         private void Update()
         {
+            if(Mathf.Approximately(healthComponent.GetFraction(), 0) || Mathf.Approximately(healthComponent.GetFraction(), 1))
+            {
+                rootCanvas.enabled = false;
+                return;
+            }
+
+            rootCanvas.enabled = true;
             foreground.localScale = new Vector3(healthComponent.GetFraction(), 1f, 1f);
         }
     }
