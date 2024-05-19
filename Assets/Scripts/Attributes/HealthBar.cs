8d1f8e274b668dc5e32fe69939bfc49206a36fff
정영일 <bbangwon@outlook.com>
2024-05-19 20:53:31 +0900

    체력바 스케일 업데이트

---
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace RPG.Attributes
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] Health healthComponent;
+        [SerializeField] RectTransform foreground;
+
+        private void Update()
+        {
+            foreground.localScale = new Vector3(healthComponent.GetFraction(), 1f, 1f);
+        }
+    }
+}
