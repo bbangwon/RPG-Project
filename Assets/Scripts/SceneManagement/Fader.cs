e2a82b09401a4f82212f9c0877fd2fd38e29e532
youngil <youngil@placeb.com>
2024-05-21 10:10:43 +0900

    Fader 고치기

--- Assets/Scripts/SceneManagement/Fader.cs
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -6,6 +6,7 @@ namespace RPG.SceneManagement
     public class Fader : MonoBehaviour
     {
         CanvasGroup canvasGroup;
+        Coroutine currentActiveFade = null;
 
         private void Awake()
         {
@@ -20,11 +21,31 @@ namespace RPG.SceneManagement
             Debug.Log("Faded in");
         }
 
+        public IEnumerator FadeIn(float time)
+        {
+            return Fade(0f, time);
+        }
+
         public IEnumerator FadeOut(float time)
         {
-            while (canvasGroup.alpha < 1)
+            return Fade(1f, time);
+        }
+
+        public IEnumerator Fade(float target, float time)
+        {
+            if (currentActiveFade != null)
             {
-                canvasGroup.alpha += Time.deltaTime / time;
+                StopCoroutine(currentActiveFade);
+            }
+            currentActiveFade = StartCoroutine(FadeRoutine(target, time));
+            yield return currentActiveFade;
+        }
+
+        private IEnumerator FadeRoutine(float target, float time)
+        {
+            while (!Mathf.Approximately(canvasGroup.alpha, target))
+            {
+                canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, target, Time.deltaTime / time);
                 yield return null;
             }
         }
@@ -34,7 +55,9 @@ namespace RPG.SceneManagement
             canvasGroup.alpha = 1;
         }
 
-        public IEnumerator FadeIn(float time)
+
+
+        private IEnumerator FadeInRoutine(float time)
         {
             while (canvasGroup.alpha > 0)
             {
