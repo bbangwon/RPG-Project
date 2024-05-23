b4c721d366a51f0527f35f2533e51937c4f877a9
youngil <youngil@placeb.com>
2024-05-23 20:00:27 +0900

    붐 마이크 오디오 리스너

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] UnityEvent onHit;
+
         public void OnHit()
         {
-            Debug.Log("Weapon Hit" + gameObject.name);
+            onHit.Invoke();
         }
     }
 
